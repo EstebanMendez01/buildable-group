49057d3ee0d69a6d933a4d9884d6dd24862f0a26
Esteban Mendez <mendez01@allegheny.edu>
2023-06-20 19:32:44 -0400

    Changes to Main.js to try and access s3

--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,47 @@
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { AppBar, Toolbar, Typography, Button, List, ListItem, ListItemText } from "@mui/material";
 import { Box } from "@mui/system";
+import AWS from 'aws-sdk';
 
+export default function Main({ user, handleLogout }) {
+  const listObjects = async () => {
+    try {
+      AWS.config.update({
+        region: "us-east-1",
+        credentials: new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: "us-east-1_lDGACWtDp",
+        }),
+      });
 
-export default function Main({user, handleLogout}) {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              AWS Cognito
-            </Typography>
+      const s3 = new AWS.S3();
+      const response = await s3.listObjects({ Bucket: "storage-buildable" }).promise();
+      console.log(response.Contents);
+    } catch (error) {
+      console.error("Error listing objects:", error);
+    }
+  };
 
-            <Typography sx={{mr: 1}} color="inherit">Hello, {user.getUsername()}</Typography>
-            <Button variant="outlined" color="inherit" onClick={handleLogout}>Logout</Button>
-          </Toolbar>
-        </AppBar>
-        <Typography variant="h2">Buildable</Typography>
-      </Box>
-    )
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            AWS Cognito
+          </Typography>
+          <Typography sx={{ mr: 1 }} color="inherit">
+            Hello, {user.getUsername()}
+          </Typography>
+          <Button variant="outlined" color="inherit" onClick={handleLogout}>
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Typography variant="h2">Secret Area</Typography>
+      <Button variant="outlined" color="primary" onClick={listObjects}>
+        List Objects in S3 Bucket
+      </Button>
+      <List>
+        {/* Render the list of objects here */}
+      </List>
+    </Box>
+  );
 }
